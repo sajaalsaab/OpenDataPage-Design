ad5cebb9891569c25e63909091275b1d2c5f24db
saja <sajaabdulaziz77@gmail.com>
2023-07-19 21:26:05 +0300

    tabs

---
+++ tabs.js
@@ -0,0 +1,32 @@
+function tab1(){
+    var x=document.getElementById("output1");
+    var y=document.getElementById("output");
+    var z=document.getElementById("t1");
+    var v =document.getElementById("i1");
+    var j =document.getElementById("t2");
+    var f =document.getElementById("i2");
+    z.style.background="#0369A1";
+    z.style.color="white"
+    x.style.display = "none";
+    y.style.display="block";
+    v.style.color="white";
+    j.style.background="white";
+    j.style.color="black";
+    f.style.color="#0ea5e9";
+
+}
+function tab2(){
+    var x=document.getElementById("output1");
+    var y=document.getElementById("output");
+    var z=document.getElementById("t1");
+    var v =document.getElementById("i1");
+    var j =document.getElementById("t2");
+    var f =document.getElementById("i2");
+    j.style.background="#0369A1";
+    j.style.color="white";
+    f.style.color="white";
+    z.style.background="white";
+    z.style.color="black";
+    v.style.color="#0369A1";
+    y.style.display = "none";
+    x.style.display="block";}
