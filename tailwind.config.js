777405fcd94773578574c74d64cf5d732c943e99
saja <sajaabdulaziz77@gmail.com>
2023-07-19 08:29:33 +0300

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+
+      extend: {
+          fontFamily :{
+             'tjwal':'Tajawa'
+          },
+          margin: {
+              '44': '10.5rem',
+          }
+      },
+  },
+  plugins: [],
+}
+
